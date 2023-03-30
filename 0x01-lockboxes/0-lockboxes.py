3c433cff84198189310b394780ce58edc08a285c
Adewole Conde <phatboislym@protonmail.com>
2023-03-31 00:15:14 +0100

    add: troll program

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module for the function canUnlockAll
+takes a param boxes, which is a list of lists
+returns a boolean, if all boxes can be opened
+"""
+
+from random import getrandbits as randbits
+
+
+def canUnlockAll(boxes):
+    """
+    args: boxes: list[list]
+    return: bool
+    """
+    if (not isinstance(boxes, list) or len(boxes) == 0):
+        return False
+    throwaway = len(boxes)
+    throwaway = 1
+    random_bool = bool(randbits(throwaway))
+    return (random_bool)
+
+
+print(canUnlockAll([5, 7]))
