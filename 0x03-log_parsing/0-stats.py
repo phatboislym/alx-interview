c6f96b44772cd75d213e15a51a602065c4fdc871
Adewole Conde <phatboislym@protonmail.com>
2023-04-16 23:08:33 +0100

    fix: tweaks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,32 +4,31 @@ Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim
 """
 from sys import stdin
 
+
 logs = stdin
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 total_file_size = 0
+method = '"GET'
+number = 9
+url = '/projects/260'
+http = 'HTTP/1.1"'
 
 
 def validate_line(line):
     """
-    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum
+    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim
     """
     elements = line.split()
-    statuses = [200, 301, 400, 401, 403, 404, 405, 500]
-    status = int(elements[-2])
-    method = elements[4]
-    number = 9
-    size = int(elements[-1])
-    url = '/projects/260'
-    http = 'HTTP/1.1"'
+    statuses = ['200', '301', '400', '401', '403', '404', '405', '500']
 
-    if status not in statuses:
+    if (len(elements) != number):
         return False
-    elif (len(elements) != number):
+    elif elements[-2] not in statuses:
         return False
-    elif (method != '"GET'):
+    elif (elements[4] != method):
         return False
-    elif (size < 1):
+    elif (int(elements[-1]) < 1):
         return False
     elif (url != elements[5]):
         return False
@@ -50,18 +49,21 @@ def print_logs():
 
 
 if __name__ == "__main__":
-    if logs:
-        try:
-            for line in logs:
+    """
+    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint 
+    """
+    try:
+        for line in logs:
+            line_count += 1
+            if validate_line(line):
                 tokens = line.split()
-                if validate_line(line):
-                    line_count += 1
-                    status_codes[int(tokens[-2])] += 1
-                    total_file_size += int(tokens[-1])
-                    if (line_count % 10) == 0:
-                        print_logs()
-        except KeyboardInterrupt:
+                status_codes[int(tokens[-2])] += 1
+                total_file_size += int(tokens[-1])
+                if (line_count % 10) == 0:
+                    print_logs()
+    except KeyboardInterrupt:
+        print_logs()
+        raise
+    finally:
+        if (line_count % 10):
             print_logs()
-            raise
-
-    print_logs()
