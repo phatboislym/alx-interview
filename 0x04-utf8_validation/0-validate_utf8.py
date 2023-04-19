1f5ab8fa2fac8a1f98ab170d34e584e5516a0e4d
Adewole Conde <phatboislym@protonmail.com>
2023-04-19 21:10:18 +0100

    fix: inverted controls

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,17 +10,12 @@ def validUTF8(data):
     args:   data
     return: bool
     """
-    blank = []
-    if data:
-        if (all(isinstance(x, int) for x in data)):
-            for number in data:
-                if (number < 0) or (number > 255):
-                    return (False)
-                else:
-                    if (number == 0):
-                        blank.append(number)
-                    else:
-                        blank.clear()
-                if len(blank) == 8:
-                    return (False)
+    if not data:
+        return (False)
+    elif not (all(isinstance(x, int) for x in data)):
+        return (False)
+    else:
+        for number in data:
+            if (number < 0) or (number > 255):
+                return (False)
     return (True)
