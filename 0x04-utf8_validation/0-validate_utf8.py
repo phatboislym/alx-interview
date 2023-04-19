3d98e41a699a16a356b2c5a58614bb67337025fc
Adewole Conde <phatboislym@protonmail.com>
2023-04-19 21:40:53 +0100

    fix: check byte patterns

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -18,12 +18,23 @@ def validUTF8(data):
         for number in data:
             if (number < 0) or (number > 255):
                 return (False)
+    bits = []
     bit_sequence = ''
     for datum in data:
         bit = bin(datum)
         bit = bit[2:]
         bit_sequence += bit
+        bits.append(bit)
     for i in range(len(bit_sequence)):
-        if bit_sequence[i:(i+8)] == '00000000':
+        if bit_sequence[i:(i + 8)] == '00000000':
+            return (False)
+    for i in range(len(bits)):
+        if bits[i][:2] == '10' and bits[-1][:2] != '11':
+            return (False)
+        elif bits[i][:2] == '11' and bits[(i + 1)][:2] != '10':
+            return (False)
+        elif bits[i][:3] == '111' and bits[(i + 2)][:2] != '10':
+            return (False)
+        elif bits[i][:4] == '1111' and bits[(i + 3)][:2] != '10':
             return (False)
     return (True)
