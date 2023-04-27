2e66375c18dd728e70d46dd03cb519116e962b94
Adewole Conde <phatboislym@protonmail.com>
2023-04-28 00:10:02 +0100

    add: task 0

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+module for function `nqueens`
+"""
+import random
+import sys
+
+
+def nqueens(n):
+    """
+    args:   n: int
+    return: True|False
+    """
+    n = int(n)
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+    elif (n < 4):
+        print("N must be at least 4")
+        sys.exit(1)
+    else:
+        x = random.randint(0, 100)
+        if (x % 2):
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    else:
+        nqueens(sys.argv[1])
