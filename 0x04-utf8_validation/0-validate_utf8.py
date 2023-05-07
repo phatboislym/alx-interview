bc339dbff5f7d28dfb479b07cb1530d1499c7c18
Adewole Conde <phatboislym@protonmail.com>
2023-05-07 23:35:33 +0100

    sync

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,9 +10,9 @@ def validUTF8(data):
     args:   data
     return: True | False
     """
-    valid_range = range(256)
     size = len(data)
     bits = []
+    too_many_ones = '11111'
     two_byte_prefix = '110'
     three_byte_prefix = '1110'
     four_byte_prefix = '11110'
@@ -23,37 +23,45 @@ def validUTF8(data):
     elif not (all(isinstance(x, int) for x in data)):
         return (False)
     for datum in data:
-        if (datum not in valid_range):
-            return (False)
-        bit = format(datum, '08b')
+        bit = format(datum, '08b')[-8:]
         bits.append(bit)
 
     for i in range(size):
+        # check if bytes is a single byte character
+        if (bits[i][:1] == '0'):
+            continue
+        # check if byte has more than the max number of leading on bits
+        elif (bits[i][:5] == too_many_ones):
+            return (False)
         # check for a valid four-byte character
-        if (bits[i][:5] == four_byte_prefix and (i + 3) <= size):
+        elif (bits[i][:5] == four_byte_prefix and (i + 3) < size):
             if ((bits[i + 1][:2] == continuation_bits) and
                 (bits[i + 2][:2] == continuation_bits) and
                     (bits[i + 3][:2] == continuation_bits)):
                 i += 3
+                continue
             else:
                 return (False)
         # check for a valid three-byte character
-        elif (bits[i][:4] == three_byte_prefix and (i + 2) <= size):
+        elif (bits[i][:4] == three_byte_prefix and (i + 2) < size):
             if ((bits[i + 1][:2] == continuation_bits) and
                     (bits[i + 2][:2] == continuation_bits)):
                 i += 2
+                continue
             else:
                 return (False)
         # check for a valid two-byte character
-        elif ((bits[i][:3] == two_byte_prefix) and ((i + 1) <= size)):
+        elif ((bits[i][:3] == two_byte_prefix) and ((i + 1) < size)):
             if (bits[i + 1][:2] == continuation_bits):
                 i += 1
+                continue
             else:
                 return (False)
-    # check if a byte with continuation_bits is preceded by valid start bytes
+        # check if a byte with continuation_bits is preceded by a start
+        # or continuation byte
         elif (bits[i][:2] == continuation_bits):
             if ((bits[i - 1][:3] != two_byte_prefix) and
-                    (bits[i - 2][:4] != three_byte_prefix) or
-                    (bits[i - 3][:5] != four_byte_prefix)):
+                    (bits[i - 2][:2] != continuation_bits)):
                 return (False)
+
     return (True)
